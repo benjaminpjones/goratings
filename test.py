a550a0b5716bd1ffb85cc48e528e8a5e6f25c235
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2023-10-29 10:24:50 -0700

    Add labels and stuff

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from goratings.math.glicko2 import Glicko2Entry, glicko2_update
-from analysis.util.RatingMath import rating_to_rank, rank_to_rating
 from math import exp, log
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 from random import choices
 
 START_RANK = 1150
@@ -14,36 +14,8 @@ def rank_to_rating(rank):
 def rating_to_rank(rating):
     return log(rating / A) * C
 
+RATING_5K = rank_to_rating(25)
 RATING_25K = rank_to_rating(5)
-
-# this is a user who only plays (established) SDK
-user = Glicko2Entry(START_RANK, START_DEVIATION)
-ranks = []
-for i in range(300):
-    rank = rating_to_rank(user.rating)
-    ranks.append(rank)
-    user = glicko2_update(user, [(Glicko2Entry(rating=1500, deviation=65), 0)])
-
-# this is a user who plays evenly ranked players, but won their first 10 matches
-user2 = Glicko2Entry(START_RANK, START_DEVIATION)
-ranks2 = []
-for i in range(290):
-    rating = user2.rating
-    rank = rating_to_rank(user2.rating)
-    ranks2.append(rank)
-
-    odds = 10 ** (abs(rating - RATING_25K) / 400) 
-    RATING_25K = rank_to_rating(5)
-    if rating > RATING_25K:
-        winrate = 1 / (odds + 1)
-    else:
-        winrate = odds / (odds + 1)
-
-    result = choices([1, 0], [winrate, 1-winrate])[0]
-    print(result)
-    
-    user2 = glicko2_update(user2, [(Glicko2Entry(rating=user2.rating, deviation=65), result)])
-    print(user2)
 
 def rand_result(winrate):
     return choices([1, 0], [winrate, 1-winrate])[0]
@@ -70,8 +42,7 @@ def simulate(result_function):
         user = glicko2_update(user, [result])
     return ranks
 
-def winrate(rank1, rank2):
-    rating1, rating2 = rank_to_rating(rank1), rank_to_rating(rank2)
+def winrate(rating1, rating2):
     odds = 10 ** (abs(rating1 - rating2) / 400)
     if rating1 > rating2:
         return odds / (odds + 1)
@@ -79,34 +50,53 @@ def winrate(rank1, rank2):
         return 1 / (odds + 1)
 
 def only_plays_5k_and_never_wins(game_info):
-    rating_5k = rank_to_rating(25)
-    return (Glicko2Entry(rating_5k, 65), 0)
+    return (Glicko2Entry(RATING_5K, 65), 0)
 
 def plays_5k_and_wins_occasionally(game_info):
     rating_5k = rank_to_rating(25)
-    return (Glicko2Entry(rating_5k, 65), rand_result(winrate(5, 25)))
+    return (Glicko2Entry(RATING_5K, 65), rand_result(winrate(RATING_25K, RATING_5K)))
 
 def plays_proper_matches(game_info):
-    rank = game_info["rank"]
-    result = rand_result(winrate(5, rank))
-    return (Glicko2Entry(game_info["rating"], 65), result)
+    rating = game_info["rating"]
+    result = rand_result(winrate(RATING_25K, rating))
+    return (Glicko2Entry(rating, 65), result)
 
 def wins_a_few_at_the_start(game_info):
     i = game_info["i"]
-    if i < 3:
+    if i < 1:
         return (Glicko2Entry(game_info["rating"], 65), 1)
     else:
         return plays_proper_matches(game_info)
 
+def simulate_averaged(f):
+    """Just like simulate, but attempts to mitigate randomness through averaging"""
+    trials = 100
+    rank_totals = [0] * 300
+    for i in range(trials):
+        ranks = simulate(f)
+        for i in range(300):
+            rank_totals[i] += ranks[i]
+    return [ tot / trials for tot in rank_totals ]
+
 result_functions = [
-    only_plays_5k_and_never_wins,
-    plays_proper_matches,
-    wins_a_few_at_the_start,
-    plays_5k_and_wins_occasionally,
+    # only_plays_5k_and_never_wins,
+    ("Plays proper rank", plays_proper_matches),
+    ("Wins 3 at start", wins_a_few_at_the_start),
+    ("Plays only 5k", plays_5k_and_wins_occasionally),
 ]
 
+
 for f in result_functions:
-    plt.plot(simulate(f))
+    line, = plt.plot(simulate_averaged(f[1]))
+    line.set_label(f[0])
+
+@mticker.FuncFormatter
+def kyu_formatter(rank, pos):
+    return f"{int(30-rank)}k"
+
+plt.gca().yaxis.set_major_formatter(kyu_formatter)
+plt.gca().legend()
+
 plt.show()
 
 print(rating_to_rank(400))
